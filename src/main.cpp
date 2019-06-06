30bec93c0de884dd9db1d32c2a44257344059d3e
MkLHX <mickael.lehoux@gmail.com>
2019-06-06 17:51:16 +0200

    update example comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 #include "Adafruit_ADS1015.h"
 #include "DFRobot_ESP_EC.h"
+#include "EEPROM.h"
 
 DFRobot_ESP_EC ec;
 Adafruit_ADS1115 ads;
@@ -10,7 +11,8 @@ float voltage, ecValue, temperature = 25;
 void setup()
 {
 	Serial.begin(115200);
-	ec.begin();
+	EEPROM.begin(32);//needed EEPROM.begin to store calibration k in eeprom
+	ec.begin();//by default lib store calibration k since 10 change it by set ec.begin(30); to start from 30
 	ads.setGain(GAIN_ONE);
 	ads.begin();
 }
@@ -20,11 +22,12 @@ void loop()
 	static unsigned long timepoint = millis();
 	if (millis() - timepoint > 1000U) //time interval: 1s
 	{
+
 		timepoint = millis();
-		voltage = ads.readADC_SingleEnded(0)/10;
+		voltage = ads.readADC_SingleEnded(0) / 10;
 		Serial.print("voltage:");
 		Serial.println(voltage, 4);
-		
+
 		//temperature = readTemperature();  // read your temperature sensor to execute temperature compensation
 		Serial.print("temperature:");
 		Serial.print(temperature, 1);
